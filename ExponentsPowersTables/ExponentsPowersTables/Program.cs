951339f8a8fbd0b89d34d667abdbfb0d282fc4df
Kjacksonii <82187409+Kjacksonii@users.noreply.github.com>
2021-05-27 00:07:51 -0400

    Add files via upload

---
+++ ExponentsPowersTables/ExponentsPowersTables/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ExponentsPowersTables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int foo;
+            bool userContinues = false;
+
+            Console.WriteLine("Enter an integer: ");
+            foo = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= foo; i++)
+            {
+
+                Console.WriteLine(i + "\t" + i * i + "\t" + i * i * i);
+
+
+            }
+            Console.WriteLine("would you like to continue? y/n");
+            string userResponse = Console.ReadLine();
+            
+            if (userResponse == "y")
+            {
+                userContinues = true;
+                while (userContinues)
+                {
+                    Console.WriteLine("Enter an integer: ");
+                    foo = Convert.ToInt32(Console.ReadLine());
+
+
+                    for (int i = 1; i <= foo; i++)
+                    {
+
+                        Console.WriteLine(i + "\t" + i * i + "\t" + i * i * i);
+
+
+                    }
+
+                    Console.WriteLine("would you like to continue? y/n");
+                    userResponse = Console.ReadLine();
+                    if (userResponse == "n")
+                    {
+                        userContinues = false;
+                    }
+                }
+            }
+
+        }
+    }
+}
+
+
+
